a1f43e8e4fa0f8ab79036c3990b543496254b1c3
Shane Orend <wagen00@gmail.com>
2017-10-11 11:21:52 -0400

    Almost done

--- app.js
+++ app.js
@@ -1,26 +1,56 @@
-var log = require('single-line-log').stdout;
-
-
 var userGuess = process.argv[2];
-function Word() {
-	
+function Word(src) {
+	this.src = src;
+	this.allLetters = [];
+	//loop through src string th
+	for (var i = 0; i < src.length; i++) {
+		var letter = new Letter(src[i]);
+		this.allLetters.push(letter);
+	};
+	this.displayWord =function () {
+		var output = "";
+		for (var i = 0; i < this.allLetters.length; i++) {
+			var lettr = word.allLetters[i];
+			output = output + lettr.displayChar() + " ";
+		};
+		return output;
+	};
 };
-function Letter(){
+function Letter(trueValue){
+// is the letter guessed
+	this.trueValue=trueValue;
+	this.guessed = false;
+	this.displayChar = function(num){
+		if (this.guessed){
+			return this.trueValue;
 
+		}
+		else{
+			return "_";
+		}
+	};
+	
 };
 console.log("Guess a letter");
 
-var wordsToGuess = ["lt","rt","lt"];
+var wordsToGuess = ["ltdsfsdfd","rbfbt","lddt"];
 var K =  Math.floor(Math.random() * wordsToGuess.length);
+var word = new Word(wordsToGuess[K]);
+console.log(word.displayWord());
+// var splitEm = wordsToGuess[K].split("");
+// console.log(splitEm);
 
-var splitEm = wordsToGuess[K].split("");
-console.log(splitEm);
-for (var i = 0; i < splitEm.length; i++) {
-	console.log("_");
-};
-if(userGuess === indexOF(splitEm)) {
-            console.log("Kick ass dude")
-        }
-else {console.log("YouSuck")}        
+
+
+// for (var i = 0; i < word.allLetters.length; i++) {
+// 	var lettr = word.allLetters[i];
+// 	console.log(lettr.displayChar());
+// };
+// console.log()
+// if(userGuess === splitEm) {
+//             console.log("Nice dude")
+//         }
+// else {console.log("YouSuck")}   
+
 
 
